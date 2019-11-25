c332dfd7c2e741eb02607211fd1d7f30546a4295
WillieXia <WillieXia@users.noreply.github.com>
2019-11-25 03:52:56 -0500

    Optimizations

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import numpy
 import tensorflow as tf
 import keras
-from keras.models import Sequential
+import sys
 from keras.layers import Dense
 from keras.layers import Dropout
-from keras.layers import LSTM
-from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
 #Setting up the text
@@ -19,58 +17,55 @@ int_to_char = dict((i, c) for i, c in enumerate(char_map))
 char_to_int = dict((c, i) for i, c in enumerate(char_map))
 
 n_vocab = len(char_map)
+n_chars = len(huckle_raw)
 
 breaksection = 100
 Xdata = []
 Ydata= []
-for i in range(0, len(huckle_raw)-breaksection, 1):
+for i in range(0, n_chars-breaksection, 1):
     seq_in = huckle_raw[i:i + breaksection]
     seq_out = huckle_raw[i+breaksection]
     Xdata.append([char_to_int[char] for char in seq_in])
     Ydata.append(char_to_int[seq_out])
-  
+ 
 #Reshaping and normalizing
-X = numpy.reshape(Xdata, (len(Xdata), breaksection, 1))
-X = X / float(len(char_map))
-Y = np_utils.to_categorical(Ydata)
+Xp = numpy.reshape(Xdata, (len(Xdata), breaksection, 1))
+Xp = Xp / float(n_vocab)
+Yp = np_utils.to_categorical(Ydata)
 
 model = keras.models.Sequential()
-model.add(keras.layers.LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(keras.layers.LSTM(256, input_shape=(Xp.shape[1], Xp.shape[2])))
 model.add(keras.layers.Dropout(0.2))
-model.add(keras.layers.Dense(Y.shape[1], activation='softmax'))
+model.add(keras.layers.Dense(Yp.shape[1], activation='softmax'))
+
+filename = "huckleberry-model-05-1.9205.hdf5"
+model.load_weights(filename)
+
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-
-
-##Saving checkpoint Models
-#filepath="Huckleberry-Model-{epoch:02d}-{loss:.4f}.hdf5"
-#checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+###saving checkpoint models
+#filepath="huckleberry-model-{epoch:02d}-{loss:.4f}.hdf5"
+#checkpoint = keras.callbacks.ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 #callbacks_list = [checkpoint]
 
-##Fitting the model/#Tests
-#model.fit(X, Y, epochs=20, batch_size=128, callbacks=callbacks_list)
+###fitting the model/#tests
+#model.fit(Xp, Yp, epochs=20, batch_size=128, callbacks=callbacks_list)
 
-
-#Testing the Model
-filename = "Huckleberry-Model-20-1.9638.hdf5"
-model.load_weights(filename)
-model.compile(loss='categorical_crossentropy', optimizer='adam')
 # pick a random seed
-start = numpy.random.randint(0, len(X)-1)
-pattern = X[start]
+start = numpy.random.randint(0, len(Xdata)-1)
+pattern = Xdata[start]
 print ("Seed:")
-print(pattern)
-print(len(pattern))
+concat = "/"
 print ("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
 # generate characters
 for i in range(1000):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
-	x = x / float(n_vocab)
-	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
-	result = int_to_char[index]
-	seq_in = [int_to_char[value] for value in pattern]
-	sys.stdout.write(result)
-	pattern.append(index)
-	pattern = pattern[1:len(pattern)]
+    xout = numpy.reshape(pattern, (1, len(pattern), 1))
+    xout = xout / float(n_vocab)
+    prediction = model.predict(xout, verbose=0)
+    index = numpy.argmax(prediction)
+    result = int_to_char[index]
+    seq_in = [int_to_char[value] for value in pattern]
+    sys.stdout.write(result)
+    pattern.append(index)
+    pattern = pattern[1:len(pattern)]
 print ("\nDone.")
